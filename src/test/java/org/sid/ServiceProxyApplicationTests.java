ea3df8f01c420b356026b455926925631307c765
elayane zakaria <zakaria15elyane@gmail.com>
2020-12-06 19:23:03 +0100

    first commit

---
+++ src/test/java/org/sid/ServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
